54ca54a2dc252b9bf215e329438137a6f9cd5c1e
leekmikan <139122626+leekmikan@users.noreply.github.com>
2023-07-25 16:34:53 +0900

    Add files via upload

--- gahaku/main.js
+++ gahaku/main.js
@@ -1,5 +1,3 @@
-﻿//620*685
-var karuta = false;
 var ms = new Audio();
 ms.preload = "auto";
 ms.src = "snd/TENRANKAI.mp3";
@@ -11,27 +9,23 @@ ms.addEventListener("ended", function () {
 var rand = 0;
 var playing = false;
 function Ans(){
-	if((qs[rand].ans == document.getElementById("ans").value && !karuta) || (qs2[rand] == document.getElementById("ans").value && karuta)){
+	if(qs[rand].ans == document.getElementById("ans").value){
 		alert("正解!");
 		Change(rd());
 	}
 }
 function rd(){
-	return karuta ? Math.floor( Math.random() * qs2.length ) : Math.floor( Math.random() * qs.length );
+	return Math.floor( Math.random() * qs.length );
 }
 function Change(x){
 	rand = x;
 	document.getElementById("ans").value = "";
-	if(!karuta){
-		document.getElementById("no").innerText = "No." + (x + 1);
-		document.getElementById("genre").innerText = qs[rand].genre;
-		document.getElementById("pic").src = "img/Q" + x + ".jpg";
-	}else{
-		document.getElementById("pic").src = "img2/Q" + x + ".jpg";
-	}
+	document.getElementById("no").innerText = "No." + (x + 1);
+	document.getElementById("genre").innerText = qs[rand].genre;
+	document.getElementById("pic").src = "img/Q" + x + ".jpg";
 }
 function Plps(){
-	if(!playing && !karuta){
+	if(!playing){
 		playing = true;
 		ms.currentTime = 0;
       	ms.play();
@@ -39,16 +33,20 @@ function Plps(){
 	}else{
 		playing = false;
 		ms.pause();
-    	ms.currentTime = 0;
+    		ms.currentTime = 0;
 		document.getElementById("mb").innerText = "BGM: OFF";
 	}
 }
 function Sel(){
 	var n = document.getElementById("sel").value;
 	if(!isNaN(n)){
-		if((0 < n && n <= qs.length && !karuta) || (0 < n && n <= qs2.length && karuta)){
-			Change(n - 1);
+		if(n <= 0){
+			n = 1;
+		}else if(qs.length < n){
+			n = qs.length;
 		}
+		Change(n - 1);
+		document.getElementById("sel").value = n - 1;
 	}
 }
 function Start(){
@@ -57,28 +55,6 @@ function Start(){
 	document.getElementById("main").style.display = "block";
 	Plps();
 	ms.currentTime = 89.7;
-}
-function Setkaruta(){ //未使用
-	karuta = true;
-	document.getElementById("pic").style.width = "384px";
-	document.getElementById("pic").style.height = "240px";
-	document.getElementById("pt").style.visibility = "hidden";
-	document.getElementById("no").style.visibility = "hidden";
-	document.getElementById("ex").style.display = "none";
-	document.getElementById("ytb").style.display = "block";
-	document.getElementById("genrex").style.visibility = "hidden";
-	document.body.style.backgroundImage = 'url(img2/tatami.jpg)';
-}
-function Setnormal(){ //未使用
-	karuta = false;
-	document.getElementById("pic").style.width = "800px";
-	document.getElementById("pic").style.height = "800px";
-	document.getElementById("pt").style.visibility = "visible";
-	document.getElementById("no").style.visibility = "visible";
-	document.getElementById("ex").style.display = "block";
-	document.getElementById("ytb").style.display = "none";
-	document.getElementById("genrex").style.visibility = "visible";
-	document.body.style.backgroundImage = '';
 }
 document.getElementById("ques_val").innerText = qs.length;
 document.getElementById("nol").innerText = Math.round(qs.length * 0.4);
