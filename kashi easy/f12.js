84f4862ae6826e88290899f443549deaa4238160
leekmikan <139122626+leekmikan@users.noreply.github.com>
2023-09-27 00:09:54 +0900

    No f12

---
+++ kashi easy/f12.js
@@ -0,0 +1,52 @@
+//x/10s
+const HUSEI = 30; //不正までのカウント数
+const WR = true; //事前警告あり,なし
+const OUT_WIDTH_RATIO = 0.87 //変えない方がいい(多分)
+var count = 0;
+var ran = false;
+var wr_show = false;
+
+let zoom_level = window.devicePixelRatio || window.screen.availWidth / document.documentElement.clientWidth;
+setInterval(function(){
+	if(window.outerWidth * OUT_WIDTH_RATIO > window.innerWidth * zoom_level){
+		count++;
+		if(WR && !wr_show){
+			wr_show = true;
+			Show_Wr();
+		}
+	}else if(wr_show){
+		Delete_Wr();
+		wr_show = false;
+		count = 0;
+	}else{
+		count = 0;
+	}
+	if(count > HUSEI && !ran){
+		Delete_Elements();
+	}
+}, 100);
+function Delete_Elements(){
+	let body_element = document.body;
+	while(body_element.firstChild){
+  		body_element.removeChild(body_element.firstChild);
+	}
+	var newElement = document.createElement("p");
+	var newContent = document.createTextNode("不正を検知しました。開発者ツールを閉じた状態でリロードしてください。"); 
+	newElement.appendChild(newContent);
+	body_element.insertBefore(newElement, document.body.firstChild);
+	ran = true;
+	open('about:blank', '_self').close(); //不正後も開発者ツールを触らせたくない時用
+}
+function Show_Wr(){
+	let body_element = document.body;
+	var newElement = document.createElement("h1");
+	newElement.setAttribute("id","wr"); //idが被らないように注意!!
+	newElement.setAttribute("style","text-align:center;");
+	var newContent = document.createTextNode("開発者ツールを閉じてください。" + Math.floor(HUSEI / 10) + "秒後にページが消えます。"); 
+	newElement.appendChild(newContent);
+	newElement.appendChild(newContent);
+	body_element.insertBefore(newElement, body_element.firstChild);
+}
+function Delete_Wr(){
+	document.getElementById("wr").remove();
+}
