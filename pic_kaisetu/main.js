05889b06317536d5d6b60ee949a5b9adf721abcc
leekmikan <139122626+leekmikan@users.noreply.github.com>
2024-05-05 15:40:23 +0900

    Add files

---
+++ pic_kaisetu/main.js
@@ -0,0 +1,7 @@
+var titles = document.getElementsByTagName("h1");
+document.getElementById("sum").innerText = document.getElementsByTagName("img").length;
+titles[1].innerText += "　(" + document.querySelectorAll('#ed img').length + ")";
+titles[2].innerText += "　(" + document.querySelectorAll('#gif img').length + ")";
+titles[3].innerText += "　(" + document.querySelectorAll('#line img').length + ")";
+titles[4].innerText += "　(" + document.querySelectorAll('#ame img').length + ")";
+titles[5].innerText += "　(" + document.querySelectorAll('#sec img').length + ")";
