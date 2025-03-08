ded18716e31eae40a1449df462bde632e80252b7
leekmikan <139122626+leekmikan@users.noreply.github.com>
2025-03-08 15:25:24 +0900

    Add html

---
+++ onci/main.js
@@ -0,0 +1,21 @@
+let qs = 1;
+let hintb = false;
+function hintc(){
+    hintb = !hintb;
+    document.getElementById("hintb").src = hintb ? "img/on.jpg" : "img/off.jpg";
+    document.getElementById("hint").innerText = hintb ? ans[qs - 1][1] : "---";
+}
+function jd(){
+    if(document.getElementById("ans").value == ans[qs - 1][0]){
+        document.getElementById("ans").value = "";
+        alert("正解！！");
+        loadq(Math.floor(ans.length * Math.random() + 1));
+    }
+}
+function loadq(x){
+    x = Math.min(ans.length,Math.max(1,x));
+    document.getElementById("ms").src = "snd/Q" + x + ".mp3";
+    qs = x;
+    document.getElementById("hint").innerText = hintb ? ans[qs - 1][1] : "---";
+    document.getElementById("qn").value = qs;
+}
