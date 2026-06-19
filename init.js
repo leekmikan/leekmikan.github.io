9b30901c7d7589355f5f28dd5e517dc78930db1b
leekmikan <139122626+leekmikan@users.noreply.github.com>
2026-06-19 23:18:08 +0900

    delete secret

--- init.js
+++ init.js
@@ -1,8 +1,3 @@
 window.onload = function(){
    document.body.style.display = "block";
-   const url = new URL(location.href);
-   const params = new URLSearchParams(url.search);
-   if(params.get("you") == "idiot"){
-      alert("なんでこのリンク踏んだのw　バカなのw");
-   }
 }
