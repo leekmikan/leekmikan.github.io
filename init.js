28097eb972db278539360c6d202c935690498288
leekmikan <139122626+leekmikan@users.noreply.github.com>
2026-06-19 23:14:39 +0900

    Add secret

--- init.js
+++ init.js
@@ -1,3 +1,8 @@
 window.onload = function(){
    document.body.style.display = "block";
+   const url = new URL(location.href);
+   const params = new URLSearchParams(url.search);
+   if(params.get("you") == "idiot"){
+      alert("なんでこのリンク踏んだのw　バカなのw");
+   }
 }
